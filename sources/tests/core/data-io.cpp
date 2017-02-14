belcour/alta

commit d9ce986b7298a67462a2d3e8aafcd5c3d638b9b4

core: Move 'load_data_from_text' as a function in "data_storage.h".

sources/core/data_storage.cpp
@@ -1,6 +1,6 @@
 /* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
 
-   Copyright (C) 2013, 2014, 2015 Inria
+   Copyright (C) 2013, 2014, 2015, 2016 Inria
 
    This file is part of ALTA.
 
@@ -23,11 +23,14 @@
 
 //using namespace alta;
 
-void alta::vertical_segment::load_data_from_text(std::istream& input,
-																					 const arguments& header,
-																					 alta::vertical_segment& result,
-																					 const arguments& args)
+void alta::load_data_from_text(std::istream& input,
+                               const alta::arguments& header,
+                               alta::vertical_segment& result)
 {
+  // FIXME: Eventually reinstate support for extra arguments when loading a
+  // file.
+  static alta::arguments args;
+
 	vec min, max ;
 	vec ymin, ymax;
 

sources/core/data_storage.h
@@ -1,6 +1,6 @@
 /* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
 
-   Copyright (C) 2013, 2014, 2015 Inria
+   Copyright (C) 2013, 2014, 2015, 2016 Inria
 
    This file is part of ALTA.
 
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include "data.h"
+#include "vertical_segment.h"
 #include "common.h"
 #include "args.h"
 
@@ -22,6 +23,13 @@ namespace alta
 
     // Write DATA to OUT in a compact binary format.
     void save_data_as_binary(std::ostream& out, const alta::data& data);
+
+
+    // Initialize DATA from the content of stream INPUT, which is in ALTA's
+    // native text format.
+    void load_data_from_text(std::istream& input,
+                             const alta::arguments& header,
+                             alta::vertical_segment &data);
 
     // Initialize DATA from the binary-formatted stream IN.
     void load_data_from_binary(std::istream& in, const alta::arguments& header, alta::data &data);

sources/core/data.h
@@ -1,6 +1,6 @@
 /* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
 
-   Copyright (C) 2013, 2014, 2015 Inria
+   Copyright (C) 2013, 2014, 2015, 2016 Inria
 
    This file is part of ALTA.
 
@@ -86,6 +86,7 @@ class data : public parametrized
 		virtual bool equals(const data& data,
 												double epsilon =
                         std::pow(1.0, -int(std::numeric_limits<double>::digits10 - 1)));
+
 
      friend void load_data_from_binary(std::istream& in, const alta::arguments& header,
                                          alta::data& data);

sources/core/vertical_segment.cpp
@@ -1,6 +1,6 @@
 /* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
 
-   Copyright (C) 2013, 2014, 2015 Inria
+   Copyright (C) 2013, 2014, 2015, 2016 Inria
 
    This file is part of ALTA.
 
@@ -91,7 +91,8 @@ void vertical_segment::load(const std::string& filename, const arguments& args)
 
 		load_data_from_binary(file, header, *this);
 	} else {
-		load_data_from_text(file, header, *this, args);
+    // FIXME: ARGS is currently ignored.
+		load_data_from_text(file, header, *this);
 	}
 
 	file.close();

sources/core/vertical_segment.h
@@ -129,6 +129,10 @@ class vertical_segment : public data
       // Get data size
       virtual int size() const ;
 
+      friend void load_data_from_text(std::istream& in,
+                                      const alta::arguments& header,
+                                      alta::vertical_segment& data);
+
    private: // method
 
   protected: // method
@@ -136,11 +140,6 @@ class vertical_segment : public data
       // Pre-allocate 'number_of_data_elements' elements for this data
       // object. Note, the input and output dimension needs to be specified.
       void initializeToZero( unsigned int number_of_data_elements );
-
-      static void load_data_from_text(std::istream& input,
-                                                      const arguments& header,
-                                                      vertical_segment& result,
-                                                      const arguments& args);
 
       //! \brief From a correct input configuration 'x' with size
       //! dimX()+dimY(), generate a vertical ! segment satisfying this object's

sources/plugins/data_interpolants/grid.cpp
@@ -1,6 +1,6 @@
 /* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
 
-   Copyright (C) 2013, 2014 Inria
+   Copyright (C) 2013, 2014, 2016 Inria
 
    This file is part of ALTA.
 
@@ -276,7 +276,8 @@ class BrdfGrid : public vertical_segment {
          arguments header = arguments::parse_header(file);
          initialize(header, false);
 
-         load_data_from_text(file, header, *this, args);
+         // FIXME: ARGS is ignored.
+         load_data_from_text(file, header, *this);
       }
 
       void save(const std::string& filename) const

commit 8c90c23bd3a8ccb3c4165d23f44cc7830a2c1591

core: 'plugins_manager::load' passes arguments to 'load_data_from_text'.
Fixes a regression introduced in d9ce986b7 whereby filtering arguments
were no longer passed to 'load_data_from_text'.

Reported by Romain Pacanowski.

sources/core/data_storage.cpp
@@ -153,12 +153,9 @@ static void read_confidence_interval(std::istream& input,
 }
 
 alta::data* alta::load_data_from_text(std::istream& input,
-                                      const alta::arguments& header)
+                                      const alta::arguments& header,
+                                      const alta::arguments& args)
 {
-  // FIXME: Eventually reinstate support for extra arguments when loading a
-  // file.
-  static alta::arguments args;
-
   vec min, max ;
   vec ymin, ymax;
 

sources/core/data_storage.h
@@ -1,6 +1,6 @@
 /* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
 
-   Copyright (C) 2013, 2014, 2015, 2016 Inria
+   Copyright (C) 2013, 2014, 2015, 2016, 2017 Inria
 
    This file is part of ALTA.
 
@@ -25,9 +25,12 @@ namespace alta
     void save_data_as_binary(std::ostream& out, const alta::data& data);
 
 
-    // Return the data read from INPUT in ALTA's native text format.
+    // Return the data read from INPUT in ALTA's native text format.  Read
+    // extra parsing parameters from ARGS.  XXX: ARGS is currently used for
+    // extra filtering, which should really happen elsewhere.
     data* load_data_from_text(std::istream& input,
-                              const alta::arguments& header);
+                              const alta::arguments& header,
+                              const alta::arguments& args = alta::arguments());
 
     // Return the data read from the binary-formatted stream IN.
     data* load_data_from_binary(std::istream& in, const alta::arguments& header);

sources/core/plugins_manager.cpp
@@ -1,7 +1,7 @@
 /* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
 
    Copyright (C) 2014, 2015 CNRS
-   Copyright (C) 2013, 2014, 2015, 2016 Inria
+   Copyright (C) 2013, 2014, 2015, 2016, 2017 Inria
 
    This file is part of ALTA.
 
@@ -486,8 +486,7 @@ ptr<data> plugins_manager::load_data(const std::string& type, std::istream& inpu
         if (header["FORMAT"] == "binary") {
             result = ptr<data>(load_data_from_binary(input, header));
         } else {
-            // FIXME: ARGS is currently ignored.
-            result = ptr<data>(load_data_from_text(input, header));
+            result = ptr<data>(load_data_from_text(input, header, args));
         }
     }
     else

sources/tests/core/data-io.cpp
@@ -91,6 +91,38 @@ static void test_simple_load_from_text()
     TEST_ASSERT(view.col(3) == Eigen::Vector3d(3., 7., 11.));
 }
 
+// Try loading a simple example from a text-format stream, with extra
+// filtering arguments.
+static void test_simple_load_from_text_filtering()
+{
+    static const char example[] = "\
+#DIM 1 3f\n\
+#VS 0\n\
+#PARAM_IN COS_TH\n\
+#PARAM_OUT RGB_COLOR\n\
+0 1 2 3\n\
+1 4 5 6\n\
+2 7 8 9\n\
+3 10 11 12\n\
+4 7 7 7\n";
+
+    std::istringstream input(example);
+
+    // Filter out elements that are not within those boundaries.
+    arguments args =
+        { { "ymin", "4" }, { "ymax", "10" }, { "max", "3" } };
+
+    auto data = dynamic_pointer_cast<vertical_segment>(
+        plugins_manager::load_data("vertical_segment", input, args));
+    TEST_ASSERT(data != NULL);
+
+    // There should be only 2 elements left after filtering.
+    TEST_ASSERT(data->size() == 2);
+
+    TEST_ASSERT(data->get(0) == Eigen::Vector4d(1., 4., 5., 6.));
+    TEST_ASSERT(data->get(1) == Eigen::Vector4d(2., 7., 8., 9.));
+}
+
 // Files that are automatically deleted upon destruction.
 class temporary_file
 {
@@ -170,8 +202,9 @@ int main(int argc, char** argv)
         input_file = data_dir + "/" + data_file;
     }
 
-    // Simple test first.
+    // Simple tests first.
     test_simple_load_from_text();
+    test_simple_load_from_text_filtering();
 
     // Try a sequence of loads and saves.
     try