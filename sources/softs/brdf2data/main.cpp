d2017072c3c528798f20702ce735c12f26b9cd29
Laurent Belcour <laurent.belcour@gmail.com>
2021-04-15 14:49:39 +0200

    Fixing the issue with brdf2data not calling correctly the data->set() function

--- sources/softs/brdf2data/main.cpp
+++ sources/softs/brdf2data/main.cpp
@@ -36,119 +36,96 @@ using namespace alta;
 
 int main(int argc, char** argv)
 {
-	arguments args(argc, argv) ;
+   arguments args(argc, argv) ;
 
-	if(args.is_defined("help")) {
-		std::cout << "Usage: brdf2data --input brdf.file --output data.file [--data exporter.so --data-file data.file]" << std::endl ;
-		std::cout << "Convert a function object to a data object."<< std::endl ;
-		std::cout << std::endl;
-		std::cout << "Mandatory arguments:" << std::endl;
-		std::cout << "  --input     [filename]" << std::endl;
-		std::cout << "  --output    [filename]" << std::endl;
-		std::cout << std::endl;
-		std::cout << "Optional arguments:" << std::endl;
-		std::cout << "  --data      [filename] Name of the data plugin used to save the output" << std::endl ;
-		std::cout << "                         data file. If no plugin is defined, the data file" << std::endl ;
-		std::cout << "                         will be load using ALTA format but require an" << std::endl;
-		std::cout << "                         ALTA file as template." << std::endl ;
-		std::cout << "  --data-file [filename] ALTA data file used as a template if no data" << std::endl ;
-		std::cout << "                         plugin is specified to export data." << std::endl ;
-		return 0 ;
-	}
+   if(args.is_defined("help")) {
+      std::cout << "Usage: brdf2data --input brdf.file --output data.file [--data exporter.so --data-file data.file]" << std::endl ;
+      std::cout << "Convert a function object to a data object."<< std::endl ;
+      std::cout << std::endl;
+      std::cout << "Mandatory arguments:" << std::endl;
+      std::cout << "  --input     [filename]" << std::endl;
+      std::cout << "  --output    [filename]" << std::endl;
+      std::cout << std::endl;
+      std::cout << "Optional arguments:" << std::endl;
+      std::cout << "  --data      [filename] Name of the data plugin used to save the output" << std::endl ;
+      std::cout << "                         data file. If no plugin is defined, the data file" << std::endl ;
+      std::cout << "                         will be load using ALTA format but require an" << std::endl;
+      std::cout << "                         ALTA file as template." << std::endl ;
+      std::cout << "  --data-file [filename] ALTA data file used as a template if no data" << std::endl ;
+      std::cout << "                         plugin is specified to export data." << std::endl ;
+      return 0 ;
+   }
 
-	if(! args.is_defined("input")) {
-		std::cerr << "<<ERROR>> the input filename is not defined" << std::endl ;
-		return 1 ;
-	}
-	if(! args.is_defined("output")) {
-		std::cerr << "<<ERROR>> the output filename is not defined" << std::endl ;
-		return 1 ;
-	}
-	if(! args.is_defined("data") && ! args.is_defined("data-file")) {
-		std::cerr << "<<ERROR>> the data exporter is not defined" << std::endl ;
-		return 1 ;
-	}
-	
-	// Get the associated data object and load the file is any
-	ptr<data> d;
+   if(! args.is_defined("input")) {
+      std::cerr << "<<ERROR>> the input filename is not defined" << std::endl ;
+      return 1 ;
+   }
+   if(! args.is_defined("output")) {
+      std::cerr << "<<ERROR>> the output filename is not defined" << std::endl ;
+      return 1 ;
+   }
+   if(! args.is_defined("data") && ! args.is_defined("data-file")) {
+      std::cerr << "<<ERROR>> the data exporter is not defined" << std::endl ;
+      return 1 ;
+   }
+   
+   // Get the associated data object and load the file is any
+   ptr<data> d;
+   if(args.is_defined("data-file")) {
+      try
+      {
+         d = plugins_manager::load_data(args["data-file"], args["data"], args);
+      }
+      CATCH_FILE_IO_ERROR(args["data-file"]);
+   } else {
+      d = plugins_manager::get_data(args["data"], 0, parameters(), args);
+   }
 
-  try
-  {
-      d = plugins_manager::load_data(args["data-file"], args["data"], args);
-	}
-  CATCH_FILE_IO_ERROR(args["data-file"]);
+   // Get the function file
+   function* f = NULL;
+   f = plugins_manager::load_function(args["input"]);
+   if(f == NULL)
+   {
+      std::cerr << "<<ERROR>> cannot open the function file" << std::endl;
+      return 1;
+   }
 
-	// Get the function file
-	function* f = NULL;
-	f = plugins_manager::load_function(args["input"]);
-	if(f == NULL)
-	{
-		std::cerr << "<<ERROR>> cannot open the function file" << std::endl;
-		return 1;
-	}
+   if(d && f != NULL)
+   {
+      const bool output_dif = args.is_defined("export-diff");
 
-  // Get the output object. In the case where it is not a VS file, we use the
-  // load object.
-  ptr<data> d_out = plugins_manager::get_data(args["data"],
-                                              d->size(),
-                                              dynamic_pointer_cast<vertical_segment>(d)
-                                              ? d->parametrization()
-                                              : f->parametrization(),
-                                              args);
+      vec temp(f->parametrization().dimX());
+      for(int i=0; i<d->size(); ++i)
+      {
+         // Copy the input vector
+         vec x = d->get(i);
 
-	if(d && f != NULL)
-	{
-		const bool output_dif = args.is_defined("export-diff");
-
-		vec temp(f->parametrization().dimX());
-		for(int i=0; i<d->size(); ++i)
-		{
-        // Copy the input vector
-        vec x = d->get(i);
-        // Convert the data to the function's input space.
-        if(f->parametrization().input_parametrization() == params::UNKNOWN_INPUT)
-        {
-            memcpy(&temp[0], &x[0], f->parametrization().dimX()*sizeof(double));
-        }
-        else
-        {
+         // Convert the data to the function's input space.
+         if(f->parametrization().input_parametrization() == params::UNKNOWN_INPUT)
+         {
+            temp = x.head(f->parametrization().dimX());
+         }
+         else
+         {
             params::convert(&x[0],
                             d->parametrization().input_parametrization(),
                             f->parametrization().input_parametrization(),
                             &temp[0]);
-        }
-        vec y = f->value(temp);
+         }
 
-        if (dynamic_pointer_cast<vertical_segment>(d_out))
-        {
-            // Vertical segment has a "scatter" representation, so we need to
-            // pass both X and Y.
-            vec z(d_out->parametrization().dimX()
-                  + d_out->parametrization().dimY());
+         // Eval BRDF
+         vec y = f->value(temp);
+         x.tail(d->parametrization().dimY()) = (output_dif) ? x.tail(d->parametrization().dimY())-y : y;
+         d->set(i, x);
+      }  
 
-            for(int j=0; j<d->parametrization().dimX(); ++j) {
-                z[j] = x[j];
-            }
-            for(int j=0; j<d->parametrization().dimY(); ++j) {
-                z[d->parametrization().dimX() + j] =
-                    output_dif ? x[d->parametrization().dimX() + j] - y[j] : y[j];
-            }
+      // Save data to file
+      d->save(args["output"]);
+   }  
+   else
+   {
+      std::cerr << "<<ERROR>> cannot import function or export data" << std::endl ;
+   }
 
-            d_out->set(i, z);
-        }
-        else
-        {
-            // Grid representation: only Y is stored.
-            d_out->set(i, y);
-        }
-		}	
-
-        d_out->save(args["output"]);
-	}	
-	else
-	{
-		std::cerr << "<<ERROR>> cannot import function or export data" << std::endl ;
-	}
-
-	return 0 ;
+   return 0 ;
 }
